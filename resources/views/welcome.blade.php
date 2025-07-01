364653d635308fd1eedd19763fb95a5761f7e03a
Panagiotis <panagiotis@e-avenue.eu>
2025-07-01 21:41:36 +0300

    Prosthiki fav icon kai redirect routes gia login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
+
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+        <link rel="icon" href="{{ asset('favicon.png') }}" type="image/πνγ">
+
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
--- routes/web.php
+++ routes/web.php
@@ -3,18 +3,21 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Όλα τα routes προστατεύονται με auth
+Route::middleware(['auth'])->group(function () {
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+    Route::get('/', function () {
+        return view('dashboard'); // κοινή αρχική σελίδα μετά το login
+    })->name('home');
 
-Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+// Φόρτωση των auth routes (login, register κ.λπ.)
 require __DIR__.'/auth.php';
