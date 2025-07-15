a2303c4d6af4650d37f8b62a6a0b636ad934a37b
Panagiotis <panagiotis@e-avenue.eu>
2025-07-16 00:48:25 +0300

    allagh diatakshs toy delete order

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -15,12 +15,6 @@
                     </div>
                     <div class="order-actions">
                         <a href="{{ route('orders.show', $order) }}" class="btn btn-dark">Προβολή</a>
-                        <form action="{{ route('orders.destroy', $order) }}" method="POST"
-                            onsubmit="return confirm('Είσαι σίγουρος;');" class="mt-3">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn btn-danger">Διαγραφή Παραγγελίας</button>
-                        </form>
 
                         @if ($order->status === 'paid')
                             <span class="btn btn-dark disabled">Η παραγγελία έχει ολοκληρωθεί</span>
@@ -29,9 +23,15 @@
                         @else
                             <form action="{{ route('orders.complete', $order) }}" method="POST" style="display:inline;">
                                 @csrf
-                                <button type="submit" class="btn btn-dark">Ολοκλήρωση</button>
+                                <button type="submit" class="btn btn-success">Ολοκλήρωση</button>
                             </form>
                         @endif
+                        <form action="{{ route('orders.destroy', $order) }}" method="POST"
+                            onsubmit="return confirm('Είσαι σίγουρος;');" style="display:inline class="mt-3">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger">Διαγραφή Παραγγελίας</button>
+                        </form>
                     </div>
                 </div>
             @endforeach
